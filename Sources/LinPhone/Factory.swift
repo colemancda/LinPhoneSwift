47e2b3962de109c53449e860dfe31275510baffb
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-07-01 07:44:06 -0500

    Added Factory

---
+++ Sources/LinPhone/Factory.swift
@@ -0,0 +1,39 @@
+//
+//  Factory.swift
+//  LinPhone
+//
+//  Created by Alsey Coleman Miller on 7/1/17.
+//
+//
+
+import CLinPhone
+
+/// `LinPhone.Factory` is a singleton object devoted to the creation of all the object of
+/// LinPhone that cannot created by `Linphone.Core` itself.
+public final class Factory {
+    
+    // MARK: - Singleton
+    
+    public static let shared = Factory()
+    
+    // MARK: - Properties
+    
+    internal let internalPointer: OpaquePointer
+    
+    // MARK: - Initialization
+    
+    deinit {
+        
+        // Clean the factory. 
+        /// This function is generally useless as the factory is unique per process,
+        /// however calling this function at the end avoid getting reports from belle-sip leak detector'
+        /// about memory leaked in linphone_factory_get().
+        linphone_factory_clean()
+    }
+    
+    /// Create the singleton factory.
+    private init() {
+        
+        self.internalPointer = linphone_factory_get()
+    }
+}
