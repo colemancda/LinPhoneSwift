040597998f6b6452199f9c97874dae6e5ec7c764
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-07-06 00:49:12 -0500

    Working on ErrorInfo

--- Sources/LinPhone/ErrorInfo.swift
+++ Sources/LinPhone/ErrorInfo.swift
@@ -27,23 +27,74 @@ public struct ErrorInfo {
     public init() {
         
         self.init(Reference())
+    }
+    
+    public init(factory: Factory = Factory.shared) {
+        
+        self.init(referencing: Reference(factory: factory))
     }*/
     
     // MARK: - Accessors
+    
+    /*
+     public var detailedErrorInfo: ErrorInfo? {
+     
+        get { return internalReference.reference.getReferenceConvertible(.copy, linphone_error_info_get_sub_error_info) }
+     
+        mutating set { internalReference.mutatingReference.setReferenceConvertible(copy: true, linphone_error_info_set_sub_error_info, newValue) }
+     }
     
     /// Get reason code from the error info.
     public var reason: Reason {
         
         @inline(__always)
         get { return Reason(linphone_error_info_get_reason(internalReference.reference.rawPointer)) }
+        
+        @inline(__always)
+        mutating set { linphone_error_info_set_reason(internalReference.reference.rawPointer, newValue.linPhoneType) }
     }
     
-    public var detailedErrorInfo: ErrorInfo? {
+    /// The protocol name.
+    public var `protocol`: String? {
         
-        get { return internalReference.reference.getReferenceConvertible(.copy, linphone_error_info_get_sub_error_info) }
+        @inline(__always)
+        get { return internalReference.reference.getString(linphone_error_info_get_protocol) }
         
-        mutating set { internalReference.mutatingReference.setReferenceConvertible(.copy, linphone_error_info_set_sub_error_info, newValue) }
+        @inline(__always)
+        mutating set { internalReference.mutatingReference.setString(linphone_error_info_set_protocol, newValue) }
     }
+    
+    /// The status code from the low level protocol (e.g. a SIP status code).
+    public var protocolCode: Int32 {
+        
+        @inline(__always)
+        get { return linphone_error_info_get_protocol_code(internalReference.reference.rawPointer) }
+        
+        @inline(__always)
+        mutating set { linphone_error_info_set_protocol_code(internalReference.reference.rawPointer, newValue) }
+    }
+    
+    /// The textual phrase from the error info. 
+    /// This is the text that is provided by the peer in the protocol (SIP).
+    public var phrase: String? {
+        
+        @inline(__always)
+        get { return internalReference.reference.getString(linphone_error_info_get_phrase) }
+        
+        @inline(__always)
+        mutating set { internalReference.mutatingReference.setString(linphone_error_info_set_phrase, newValue) }
+    }
+    
+    /// Provides additional information regarding the failure. 
+    /// With SIP protocol, the content of "Warning" headers are returned.
+    public var warnings: String? {
+        
+        @inline(__always)
+        get { return internalReference.reference.getString(linphone_error_info_get_warnings) }
+        
+        @inline(__always)
+        mutating set { internalReference.mutatingReference.setString(linphone_error_info_set_warnings, newValue) }
+    }*/
 }
 
 // MARK: - ReferenceConvertible
@@ -75,6 +126,14 @@ extension ErrorInfo: ReferenceConvertible {
         convenience init() {
             
             guard let rawPointer = linphone_error_info_new()
+                else { fatalError("Could not allocate instance") }
+            
+            self.init(ManagedPointer(BelledonneUnmanagedObject(rawPointer)))
+        }
+        
+        convenience init(factory: Factory) {
+                        
+            guard let rawPointer = linphone_factory_create_error_info(factory.rawPointer)
                 else { fatalError("Could not allocate instance") }
             
             self.init(ManagedPointer(BelledonneUnmanagedObject(rawPointer)))
