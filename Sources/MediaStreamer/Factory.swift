4597e687ca06d68eca632df7f44e2cc4d1e7dcc2
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-07-06 18:01:40 -0500

    Working on `MediaStreamer.Factory`

--- Sources/MediaStreamer/Factory.swift
+++ Sources/MediaStreamer/Factory.swift
@@ -61,4 +61,83 @@ public final class Factory {
         
         self.init(rawPointer: rawPointer)
     }*/
+    
+    // MARK: - Accessors
+    
+    /// Get number of available cpus for processing.
+    /// The factory initializes this value to the number of logicial processors available on the machine where it runs.
+    public var cpuCount: Int {
+        
+        @inline(__always)
+        get { return Int(ms_factory_get_cpu_count(rawPointer)) }
+    }
+    
+    // MARK: - Methods
+    
+    @inline(__always)
+    public func initializePlugins() {
+        
+        ms_factory_init_plugins(rawPointer)
+    }
+    
+    public func loadPlugins(from directory: String? = nil) {
+        
+        ms_factory_load_plugins(rawPointer, directory)
+    }
+    
+    public func load(_ mediaLibraries: Set<MediaLibrary>) {
+        
+        for library in mediaLibraries {
+            
+            switch library {
+            case .amr:      libmsamr_init(rawPointer)
+            case .x264:     libmsx264_init(rawPointer)
+            case .openh264: libmsopenh264_init(rawPointer)
+            case .silk:     libmssilk_init(rawPointer)
+            case .bcg729:   libmsbcg729_init(rawPointer)
+            case .webrtc:   libmswebrtc_init(rawPointer)
+            }
+        }
+    }
 }
+
+// MARK: - Supporting Types
+
+/// Libraries used with `MediaStreamer2`
+public enum MediaLibrary {
+    
+    case amr
+    case x264
+    case openh264
+    case silk
+    case bcg729
+    case webrtc
+}
+
+/// On iOS, plugins are built as static libraries so Liblinphone will not be able to load them at runtime dynamically. 
+/// Instead, you should declare their prototypes
+
+/// extern void libmsamr_init(MSFactory *factory);
+@_silgen_name("libmsamr_init")
+fileprivate func libmsamr_init(_ factory: UnsafeMutablePointer<MSFactory>)
+
+/// extern void libmsx264_init(MSFactory *factory);
+@_silgen_name("libmsx264_init")
+fileprivate func libmsx264_init(_ factory: UnsafeMutablePointer<MSFactory>)
+
+/// extern void libmsopenh264_init(MSFactory *factory);
+@_silgen_name("libmsopenh264_init")
+fileprivate func libmsopenh264_init(_ factory: UnsafeMutablePointer<MSFactory>)
+
+/// extern void libmssilk_init(MSFactory *factory);
+@_silgen_name("libmssilk_init")
+fileprivate func libmssilk_init(_ factory: UnsafeMutablePointer<MSFactory>)
+
+/// extern void libmsbcg729_init(MSFactory *factory);
+@_silgen_name("libmsbcg729_init")
+fileprivate func libmsbcg729_init(_ factory: UnsafeMutablePointer<MSFactory>)
+
+/// extern void libmswebrtc_init(MSFactory *factory);
+@_silgen_name("libmswebrtc_init")
+fileprivate func libmswebrtc_init(_ factory: UnsafeMutablePointer<MSFactory>)
+
