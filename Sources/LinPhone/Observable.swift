8fccdc124f38610729c133738ee30c80660eed5d
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-07-08 16:18:56 -0500

    Added Observable for LinPhoneManager

---
+++ Sources/LinPhone/Observable.swift
@@ -0,0 +1,84 @@
+//
+//  Observable.swift
+//  LinPhone
+//
+//  Created by Alsey Coleman Miller on 7/8/17.
+//
+//
+
+/// Pure Swift implementation of KVO and NotificationCenter.
+public final class Observable<Value> {
+    
+    // MARK: - Properties
+    
+    public internal(set) var value: Value {
+        
+        didSet {
+            
+            for observer in observers {
+                
+                observer.callback(value)
+            }
+        }
+    }
+    
+    // MARK: - Private Properties
+    
+    private var observers = [Observer<Value>]()
+    
+    private var nextID = 1
+    
+    // MARK: - Initialization
+    
+    public init(_ value: Value) {
+        
+        self.value = value
+    }
+    
+    // MARK: - Methods
+    
+    public func observe(_ observer: @escaping (Value) -> ()) -> Int {
+        
+        let identifier = nextID
+        
+        // create notification
+        let observer = Observer(identifier: identifier, callback: observer)
+        
+        // increment ID
+        nextID += 1
+        
+        // add to queue
+        observers.append(observer)
+        
+        return identifier
+    }
+    
+    @discardableResult
+    public func remove(observer: Int) -> Bool {
+        
+        guard let index = observers.index(where: { $0.identifier == observer })
+            else { return false }
+        
+        observers.remove(at: index)
+        
+        return true
+    }
+}
+
+public extension Observable where Value: ExpressibleByNilLiteral {
+    
+    convenience init() { self.init(nil) }
+}
+
+private struct Observer<Value> {
+    
+    let identifier: Int
+    
+    let callback: (Value) -> ()
+    
+    init(identifier: Int, callback: @escaping (Value) -> ()) {
+        
+        self.identifier = identifier
+        self.callback = callback
+    }
+}
