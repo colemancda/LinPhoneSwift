9b1b08eb4cda88c036de9c8540beb2c2e91bdeec
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-07-09 14:30:50 -0500

    Working on `Call.Stats`

--- Sources/LinPhone/CallStats.swift
+++ Sources/LinPhone/CallStats.swift
@@ -14,7 +14,7 @@ public extension Call {
         
         // MARK: - Properties
         
-        @_versioned // private(set) in Swift 4
+        @_versioned
         internal let internalReference: Reference
         
         // MARK: - Initialization
@@ -33,70 +33,46 @@ public extension Call {
             get { return internalReference.managedPointer.unmanagedPointer.rawPointer }
         }
         
+        /*
         /// The type of the stream the stats refer to.
         public var type: StreamType {
             
             @inline(__always)
             get { return StreamType(linphone_call_stats_get_type(rawPointer)) }
-        }
+        }*/
         
         /// The bandwidth measurement of the received stream, expressed in kbit/s, including IP/UDP/RTP headers.
         public var downloadBandwith: Float {
             
             @inline(__always)
-            get { return linphone_call_stats_get_download_bandwidth(rawPointer)  }
+            get { return linphone_call_stats_get_download_bandwidth(rawPointer) }
         }
         
         /// The bandwidth measurement of the sent stream, expressed in kbit/s, including IP/UDP/RTP headers.
         public var uploadBandwith: Float {
             
             @inline(__always)
-            get { return linphone_call_stats_get_upload_bandwidth(rawPointer)  }
+            get { return linphone_call_stats_get_upload_bandwidth(rawPointer) }
         }
     }
 }
 
 extension Call.Stats {
     
-    internal final class Reference: ManagedHandle {
+    internal final class Reference: BelledonneObjectHandle {
         
-        typealias RawPointer = Call.Stats.UnmanagedPointer.RawPointer
+        typealias RawPointer = BelledonneUnmanagedObject.RawPointer
         
         // MARK: - Properties
         
         @_versioned
-        internal let managedPointer: ManagedPointer<UnmanagedPointer>
+        internal let managedPointer: ManagedPointer<BelledonneUnmanagedObject>
         
         // MARK: - Initialization
         
-        internal init(_ managedPointer: ManagedPointer<UnmanagedPointer>) {
+        internal init(_ managedPointer: ManagedPointer<BelledonneUnmanagedObject>) {
             
             self.managedPointer = managedPointer
-        }
-    }
-}
-
-extension Call.Stats {
-    
-    typealias RawPointer = UnmanagedPointer.RawPointer
-    
-    struct UnmanagedPointer: LinPhone.UnmanagedPointer {
-        
-        let rawPointer: OpaquePointer
-        
-        @inline(__always)
-        init(_ rawPointer: UnmanagedPointer.RawPointer) {
-            self.rawPointer = rawPointer
-        }
-        
-        @inline(__always)
-        func retain() {
-            linphone_call_stats_ref(rawPointer)
-        }
-        
-        @inline(__always)
-        func release() {
-            linphone_call_stats_unref(rawPointer)
         }
     }
 }
